be13259770eec9812076f53fbc5d06611460a782
malmrashede <keigo1410@yahoo.co.jp>
2013-06-22 20:15:10 +0900

    test jquery ui

--- web.rb
+++ web.rb
@@ -64,8 +64,9 @@ __END__
 <html lang="ja">
   <head>
     <title>dsbd</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
     <link href="http://code.jquery.com/mobile/latest/jquery.mobile.min.css" rel="stylesheet" type="text/css" />
+    <link href="dsbd.css" rel="stylesheet" type="text/css" />
     <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
     <script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
   </head>
