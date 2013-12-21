3324d112ecc949db8dc54f228503e82d8273273c
malmrashede <keigo1410@yahoo.co.jp>
2013-12-22 00:20:52 +0900

    removed jquery.autopager

--- web.rb
+++ web.rb
@@ -75,8 +75,6 @@ __END__
     <link href="dsbd.css" rel="stylesheet" type="text/css" />
     <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
     <script src="http://code.jquery.com/mobile/latest/jquery.mobile.min.js"></script>
-    <script src="/jquery.autopager-1.0.0.js"></script>
-    <script><!--(function(){$.autopager({content:'.autopagerize_page_element',insertBefore:".autopagerize_insert_before"});})();--></script>
   </head>
   <body>
 
